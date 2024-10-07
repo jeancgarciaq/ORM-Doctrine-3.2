b2a59fff880a7c550ee8764ebdfb7113a876e7f5
jeancgarciaq <jcvictory@hotmail.com>
2024-10-07 01:14:33 -0400

    fix: I use __DIR__ again with the correct path

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,23 +1,27 @@
 <?php
 // config/boostrap.php
 
+namespace Config;
+
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 
 require_once 'vendor/autoload.php';
 
+
+
 // Create a simple "default" Doctrine ORM configuration for Attributes
 $config = ORMSetup::createAttributeMetadataConfiguration(
-    paths: ['src'],
+    paths: [__DIR__ . '../src'],
     isDevMode: true,
 );
 
-// or if you prefer XML
-// $config = ORMSetup::createXMLMetadataConfiguration(
-//    paths: [__DIR__ . '/config/xml'],
-//    isDevMode: true,
-//);
+	// or if you prefer XML
+	// $config = ORMSetup::createXMLMetadataConfiguration(
+	//    paths: [__DIR__ . '/config/xml'],
+	//    isDevMode: true,
+	//);
 
 // configuring the params of database
 $paramsDb = [
@@ -33,5 +37,6 @@ $connection = DriverManager::getConnection($paramsDb, $config);
 
 // obtaining the entity manager
 $entityManager = new EntityManager($connection, $config);
+
 
 ?>
